e51638fc01b8c96b8aee4ffe52850e4261db8b06
Fumiaki Yoshimatsu <fumiakiy@gmail.com>
2020-08-02 11:03:01 -0400

    Top page.

---
+++ pages/_document.js
@@ -0,0 +1,35 @@
+import Document, { Head, Main, NextScript } from 'next/document';
+import React from 'react';
+
+function Script() {
+  const raw = `
+  window.dataLayer = window.dataLayer || [];
+  function gtag() {dataLayer.push(arguments); }
+    gtag("js", new Date());
+
+    gtag("config", "UA-50700-3");
+ `
+  return React.createElement("script", { dangerouslySetInnerHTML: { __html: raw } });
+}
+
+class CustomDocument extends Document {
+  render() {
+    return (
+      <html lang="en">
+        <Head>
+          <meta charSet="utf-8" />
+          <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-50700-3"></script>
+          <Script />
+          <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@400;500;700&display=swap" rel="stylesheet" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
+
+export default CustomDocument;
---
+++ pages/index.tsx
@@ -0,0 +1,29 @@
+import Head from "next/head"
+import styles from "../styles/Home.module.css"
+
+export default function Home() {
+  return (<>
+    <Head>
+      <title>Fumiaki Yoshimatsu</title>
+      <meta name="description" content="A software engineer in NYC" />
+    </Head>
+    <div className={styles.root}>
+      <div className={styles.container}>
+        <h1>Hello, I'm Fumiaki Yoshimatsu.</h1>
+        <div className={styles.content}>
+          <div className={styles.avatar}>
+          <img src="/profile.jpg" />
+          </div>
+          <ul className={styles.links}>
+            <li><a href="https://github.com/fumiakiy">GitHub</a></li>
+            <li><a href="https://twitter.com/fumiakiy">Twitter</a></li>
+            <li><a href="https://fumiakiy.tumblr.com">tumblr</a></li>
+            <li><a href="https://medium.com/@fumiakiy">Medium</a></li>
+            <li><a href="https://linkedin.com/in/fumiakiy">LinkedIn</a></li>
+            <li><a href="resume/index.html">Resume</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </>)
+}
